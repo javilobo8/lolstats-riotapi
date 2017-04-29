260fa3b2924bda00f40093ca848dad66cf6b2cf3
Lobo <javilobo8@users.noreply.github.com>
2017-04-29 09:46:00 +0200

    feat(global) Initial commit 💩

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "env": {
+    "mocha": true
+  },
+  "rules": {
+    "object-curly-spacing": 0,
+    "global-require": 0
+  }
+};
